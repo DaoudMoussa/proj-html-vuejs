61c47e84d0645f4870249a62b2e821adeb4dfc81
Daoud <99m.daoud@gmail.com>
2020-11-30 20:18:18 +0100

    aggiunto header - e prima sezione

--- main.js
+++ main.js
@@ -1,9 +1,54 @@
 const app = new Vue({
     el: '#root',
     data: {
-        
+        navVoices: [
+            {
+                name: 'Home',
+                link: '#',
+                hasDropdown: true
+            },
+            {
+                name: 'Shop',
+                link: '#',
+                hasDropdown: true
+            },
+            {
+                name: 'About',
+                link: '#',
+                hasDropdown: false
+            },
+            {
+                name: 'Blog',
+                link: '#',
+                hasDropdown: false
+            },
+            {
+                name: 'Contact',
+                link: '#',
+                hasDropdown: false
+            },
+            {
+                name: 'Shop by brand',
+                link: '#',
+                hasDropdown: true
+            },
+        ],
+        activeNavVoice: 0,
+        hasDiscount: true
     },
     methods: {
+        getNavVoiceClasses(index) {
+            let classes = ''
 
+            if(index == this.activeNavVoice) {
+                classes = 'active';
+            }
+
+            if(this.navVoices[index].hasDropdown) {
+                classes += ' dropdown';
+            }
+
+            return classes;
+        }
     }
 });
