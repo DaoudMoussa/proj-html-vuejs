630089d8aafa76b752f879d20c3c16ef6c21a8bf
Daoud <99m.daoud@gmail.com>
2020-12-02 16:41:54 +0100

    aggiunto footer e credits

--- main.js
+++ main.js
@@ -193,6 +193,28 @@ const app = new Vue({
                 body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                 date: 'April 17th, 2020'
             },
+        ],
+        socials: [
+            {
+                name: 'Facebook',
+                link: '#',
+                iconClass: 'facebook'
+            },
+            {
+                name: 'Twitter',
+                link: '#',
+                iconClass: 'twitter'
+            },
+            {
+                name: 'Instagram',
+                link: '#',
+                iconClass: 'instagram'
+            },
+            {
+                name: 'Youtube',
+                link: '#',
+                iconClass: 'youtube'
+            },
         ]
     },
     methods: {
